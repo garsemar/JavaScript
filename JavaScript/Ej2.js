d05d12122ae88a253f121cb7765c625737b73d79
garsemar <martigarciaserra@gmail.com>
2022-03-14 09:20:11 +0100

    JavaScript Ej2

--- JavaScript/Ej2.js
+++ JavaScript/Ej2.js
@@ -15,25 +15,22 @@ function count(num){
 
 function createList(){
     list = []
-    for(i = 0; i <= 5; i++){
-        list.push(prompt())
+    for(i = 0; i <= 4; i++){
+        list.push(parseInt(prompt()))
     }
     return list
 }
 
 function validPos(list){
-    res = []
+    res = ['-', '-', '-', '-', '-']
     for(i = 0; i <= list.length-1; i++){
         for(j = 0; j <= codigoSecreto.length-1; j++){
             if(list[i] == codigoSecreto[j]){
-                console.log(list[i])
                 if(i == j){
-                    res.push('Ok')
+                    res[i] = 'Ok'
                 }else if(i != j){
-                    res.push(count(list[i]))
+                    res[i] = count(list[i])
                 }
-            }else{ // Esto no funciona
-                res.push('-')
             }
         }
     }
@@ -46,13 +43,7 @@ for(i = 1; i <= 5; i++){
     codigoSecreto.push(randomNumber(1, 10))
 }
 
-/* console.log(codigoSecreto)
-
-console.log(count(prompt()))
-
-console.log(validPos(prompt(), prompt())) */
-
-list = [1,2,3,4,5]
+list = createList()
 console.log(list)
 console.log(codigoSecreto)
 validPos(list)
