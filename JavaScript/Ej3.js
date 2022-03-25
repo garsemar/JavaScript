90492308b63ff41edc61afd3857dfd8f1f9161af
garsemar <martigarciaserra@gmail.com>
2022-03-25 09:42:23 +0100

    JavaScript Ej3.3, 3.4, 3.5

--- JavaScript/Ej3.js
+++ JavaScript/Ej3.js
@@ -1,7 +1,9 @@
-var now = new Date()
+const now = new Date()
 
-console.log(now.getDay())
+// Ej 1
+console.log(Date())
 
+// Ej 2
 if(now.getDay = 1){
     if(now.getHours() >= 11 && now.getHours() <= 12){
         if(now.getHours() == 12 && now.getMinutes() <= 10){
@@ -14,4 +16,35 @@ if(now.getDay = 1){
             console.log("no m04")
         }
     }
+    else{
+        console.log("no m04")
+    }
 }
+else{
+    console.log("no m04")
+}
+
+// Ej 3
+let cumpleD = 27
+let cumpleM = 03
+
+var dayUntil = Math.abs(now.getDate() - cumpleD) + (Math.abs((now.getMonth()+1) - cumpleM) * 30)
+var hourUntil = dayUntil*24
+var minUntil = hourUntil*60
+var segUntil = minUntil*60
+
+console.log("Days:" + dayUntil)
+console.log("Hours:" + hourUntil)
+console.log("Min:" + minUntil)
+console.log("Segs:" + segUntil)
+
+// Ej 4
+
+
+// Ej 5
+function timer(){
+    const nowTimer = new Date()
+    console.log(nowTimer.getHours()+":"+nowTimer.getMinutes()+":"+nowTimer.getSeconds())
+}
+
+setInterval(timer, 1000)
